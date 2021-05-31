f3f63f0a90613986fcfe24497838ba0025edcd5a
An Nguyen <nguyen.an.1196@gmail.com>
2021-05-31 13:12:59 -0700

    Example to access request variables

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
 const app = express();
+app.use(express.urlencoded({ extended: true }))
 
 app.get("/", function(req, res) {
-    res.send("<h1>Hello World!</h1>");
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+    res.send("Thanks for posting");
+    console.log(req.body.num1);
 });
 
 app.listen(3000, function() {
