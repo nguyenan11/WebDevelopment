49a5c77d1ffd5aca34f5304973490c2dceb3beea
An Nguyen <nguyen.an.1196@gmail.com>
2021-06-05 22:02:45 -0700

    Design country web app that take in user's input and display country's capital and currency

--- countryProject/index.js
+++ countryProject/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
 
-    const url = "https://restcountries.eu/rest/v2/name/viet";
+app.post("/", function(req, res) {
+    const country = req.body.countryName;
+
+    const url = "https://restcountries.eu/rest/v2/name/" + country;
     https.get(url, function(response) {
         console.log(response.statusCode);
 
@@ -21,22 +27,17 @@ app.get("/", function(req, res) {
         });
 
         response.on("end", function() {
-            const vietnam = JSON.parse(data);
+            const countryData = JSON.parse(data);
             // console.log(vietnam);
-            const capital = vietnam[0].capital;
-            const currency = vietnam[0].currencies[0].code;
-            console.log("Capital of Vietnam is " + capital + ", and their \
-currency is " + currency);
+            const capital = countryData[0].capital;
+            const currency = countryData[0].currencies[0].code;
+            res.write("<p>Capital of " + country + " is " + capital + "</p>");
+            res.write("<h1>Their currency is " + currency +".</h1>");
+            res.send();
         });
-
-    res.send("Up and running");
 
     });
 });
-
-
-
-
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
