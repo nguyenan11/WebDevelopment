1200c2e4443ec48cbc9a6806bcf0e0181171b16c
An Nguyen <nguyen.an.1196@gmail.com>
2021-08-07 13:48:24 -0700

    Add the starting for any JS app

--- secrets-submission/app.js
+++ secrets-submission/app.js
@@ -1,1 +1,25 @@
 //jshint esversion:6
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+  res.render("Home");
+});
+
+app.get("/login", function(req, res) {
+  res.render("Login");
+});
+
+app.get("/register", function(req, res) {
+  res.render("Register");
+});
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000.");
+});
