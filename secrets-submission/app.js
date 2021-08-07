cd17d5a3da16530fb78d5848790ce422d9507533
An Nguyen <nguyen.an.1196@gmail.com>
2021-08-07 14:13:04 -0700

    Allow User to register then send user's data to database

--- secrets-submission/app.js
+++ secrets-submission/app.js
@@ -1,12 +1,24 @@
 //jshint esversion:6
 
 const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true })
+
+// user schema
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema); // new User object
 
 app.get("/", function(req, res) {
   res.render("Home");
@@ -19,6 +31,23 @@ app.get("/login", function(req, res) {
 app.get("/register", function(req, res) {
   res.render("Register");
 });
+
+// post request on /register
+app.post("/register", function(req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err) {
+    if (err) {
+      consol.log(err);
+    } else {
+      res.render("Secrets");
+    }
+  })
+});
+
 
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
