ff2a25be00230f64f9d518dea6ad51ffd68af28d
An Nguyen <nguyen.an.1196@gmail.com>
2021-08-07 15:31:50 -0700

    Use md5 for password hashing instead of mongoose encryption

--- secrets-submission/app.js
+++ secrets-submission/app.js
@@ -1,9 +1,9 @@
 //jshint esversion:6
-
+require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -18,12 +18,6 @@ const userSchema = new mongoose.Schema ({
   email: String,
   password: String
 });
-
-// Encryptions - encryptedFields to prevent encrypt entire page
-// Put this above new User declaration
-const secret = "Somethinglongbutmaynothaveanymeaning.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
-
 
 const User = new mongoose.model("User", userSchema); // new User object
 
@@ -43,7 +37,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err) {
@@ -59,7 +53,7 @@ app.post("/register", function(req, res) {
 // validate login
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({email: username}, function(err, foundUser) {
     if (err) {
       consol.log(err);
